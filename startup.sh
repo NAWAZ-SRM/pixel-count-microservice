748698b8f2f135946aee34cff0c528dd9d1924fe
NAWAZ-SRM <sheriffnawaz641@gmail.com>
2025-03-23 17:58:12 +0530

    UI changes Made

--- startup.sh
+++ startup.sh
@@ -4,7 +4,11 @@ echo $(pwd)
 cd reactOpenSeaDrag-master
 npm run build
 cd ..
-uv run manage.py makemigrations
-uv run manage.py migrate
-uv run manage.py collectstatic
-uv run manage.py runserver
+# uv run manage.py makemigrations
+# uv run manage.py migrate
+# uv run manage.py collectstatic
+# uv run manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+python manage.py runserver
